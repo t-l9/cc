0cf8e33228338260dc5eed9e31bbabbc3e665924
Tim Lauter <timlauter@0-0.local>
2017-09-19 02:20:53 -0400

    getting all data

--- src/utils/index.js
+++ src/utils/index.js
@@ -7,15 +7,28 @@ export function makeRequest(slug) {
             return res.json();
         })
         .then(function(res) {
+            repo['id'] = res.data[0].id
+            repo['gpa'] = res.data[0].attributes.score;
             repo['latestSnapshot'] = res.data[0].relationships.latest_default_branch_snapshot.data.id
             return res;
         })
-        .then(function(json) {
-            const id = json.data[0].id
-            return getBuilds(id);
+        .then(function(res) {
+            return getFiles(repo['id'], repo['latestSnapshot']);
         })
         .then(function(res) {
-            repo['builds'] = res
+            repo['files'] = res;
+        })
+        .then(function(res) {
+            return getIssues(repo['id'], repo['latestSnapshot']);
+        })
+        .then(function(res) {
+            repo['issues'] = res;
+        })
+        .then(function(res) {
+            return getBuilds(repo['id']);
+        })
+        .then(function(res) {
+            repo['builds'] = res;
             return repo;
         })
         .catch(function(error) {
@@ -23,8 +36,51 @@ export function makeRequest(slug) {
         });
 }
 
+function getIssues(repoId, snapshot) {
+    return fetch(`https://api.codeclimate.com/v1/repos/${repoId}/snapshots/${snapshot}/issues?filter[severity]=minor`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(issues) {
+            return issues;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+
+function getFiles(repoId, snapshot) {
+    return fetch(`https://api.codeclimate.com/v1/repos/${repoId}/snapshots/${snapshot}/files`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(files) {
+
+            return files;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+
 function getBuilds(repoId) {
     return fetch(`https://api.codeclimate.com/v1/repos/${repoId}/builds`)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(build) {
+
+            return build;
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+
+function latestBuildIssues(repoId, snapshot) {
+    return fetch(`https://api.codeclimate.com/v1/repos/${repoId}/snapshots/${snapshot}/issues`)
         .then(function(res) {
             return res.json();
         })
